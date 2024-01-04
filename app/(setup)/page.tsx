5e8fc4a1340910ea8d6f55022f98f3035a830967
LAPTOP-57DG645P\sopor <j.pita.controlsgroups@gmail.com>
2024-01-04 08:44:11 -0400

    First commit

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+
+import { redirect } from "next/navigation";
+const SetupPage = async () => {
+
+    const profile = await initialProfile()
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+
+    if(server){
+        return redirect(`/servers/${server.id}`)
+    }
+
+    return <div>Create Server</div>;
+}
+ 
+export default SetupPage;
