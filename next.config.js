7354189b6f08478c935b12611781a4887405fd82
LAPTOP-57DG645P\sopor <j.pita.controlsgroups@gmail.com>
2024-01-04 23:25:22 -0400

    feat: configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+              },
+        ]
+        
+    }
+}
 
 module.exports = nextConfig
